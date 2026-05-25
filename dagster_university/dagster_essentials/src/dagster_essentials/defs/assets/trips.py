f03ab58b869c88fce380f5c6733baec02e36ebcf
dkopev <d.kopev@devnet.de>
2026-05-25 14:46:53 +0000

    added partitions

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -3,20 +3,27 @@ from dagster_essentials.defs.assets import constants
 from dagster_duckdb import DuckDBResource
 import os
 import dagster as dg
+from dagster_essentials.defs.partitions import monthly_partition
 
 # src/dagster_essentials/defs/assets/trips.py
-@dg.asset
-def taxi_trips_file() -> None:
+@dg.asset(
+    partitions_def=monthly_partition
+)
 
-    """The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal."""
+def taxi_trips_file(context: dg.AssetExecutionContext) -> None:
+  """
+      The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
+  """
 
-    month_to_fetch = '2023-03'
-    raw_trips = requests.get(
-        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
-    )
+  partition_date_str = context.partition_key
+  month_to_fetch = partition_date_str[:-3]
 
-    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
-        output_file.write(raw_trips.content)
+  raw_trips = requests.get(
+      f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+  )
+
+  with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
+      output_file.write(raw_trips.content)
 
 @dg.asset
 def taxi_zones_file() -> None:
@@ -30,30 +37,38 @@ def taxi_zones_file() -> None:
     with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
         output_file.write(raw_taxi_zones.content)
 
-@dg.asset(
-    deps=["taxi_trips_file"],
+@@dg.asset(
+  deps=["taxi_trips_file"],
+  partitions_def=monthly_partition,
 )
-def taxi_trips(database: DuckDBResource) -> None:
-    query = """
-        create or replace table taxi_trips as (
-          select
-            VendorID as vendor_id,
-            PULocationID as pickup_zone_id,
-            DOLocationID as dropoff_zone_id,
-            RatecodeID as rate_code_id,
-            payment_type as payment_type,
-            tpep_dropoff_datetime as dropoff_datetime,
-            tpep_pickup_datetime as pickup_datetime,
-            trip_distance as trip_distance,
-            passenger_count as passenger_count,
-            total_amount as total_amount
-          from 'data/raw/taxi_trips_2023-03.parquet'
-        );
-    """
+def taxi_trips(context: dg.AssetExecutionContext, database: DuckDBResource) -> None:
+  """
+    The raw taxi trips dataset, loaded into a DuckDB database, partitioned by month.
+  """
 
-    with database.get_connection() as conn:
-        conn.execute(query)
+  partition_date_str = context.partition_key
+  month_to_fetch = partition_date_str[:-3]
 
+  query = f"""
+    create table if not exists trips (
+      vendor_id integer, pickup_zone_id integer, dropoff_zone_id integer,
+      rate_code_id double, payment_type integer, dropoff_datetime timestamp,
+      pickup_datetime timestamp, trip_distance double, passenger_count double,
+      total_amount double, partition_date varchar
+    );
+
+    delete from trips where partition_date = '{month_to_fetch}';
+
+    insert into trips
+    select
+      VendorID, PULocationID, DOLocationID, RatecodeID, payment_type, tpep_dropoff_datetime,
+      tpep_pickup_datetime, trip_distance, passenger_count, total_amount, '{month_to_fetch}' as partition_date
+    from '{constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch)}';
+  """
+
+  with database.get_connection() as conn:
+      conn.execute(query)
+      
 @dg.asset(
     deps=["taxi_zones_file"]
 )
--- dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
@@ -1,9 +1,18 @@
 # src/dagster_essentials/defs/jobs.py
 import dagster as dg
+from dagster_essentials.defs.partitions import monthly_partition
+from dagster_essentials.defs.partitions import weekly_partition
 
 trips_by_week = dg.AssetSelection.assets(["trips_by_week"])
 
 weekly_update_job = dg.define_asset_job(
     name="weekly_update_job",
+    partitions_def=weekly_partition,
     selection=trips_by_week,
+)
+
+trip_update_job = dg.define_asset_job(
+    name="trip_update_job",
+    partitions_def=monthly_partition, # partitions added here
+    selection=dg.AssetSelection.all() - trips_by_week
 )
