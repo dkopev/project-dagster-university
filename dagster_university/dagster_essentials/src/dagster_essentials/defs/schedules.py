1fbbef9ce82ffe0b98de44394c6b7d435a0f4dcc
dkopev <d.kopev@devnet.de>
2026-05-25 14:11:24 +0000

    schedules add

---
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/schedules.py
@@ -0,0 +1,10 @@
+
+# src/dagster_essentials/defs/schedules.py
+import dagster as dg
+
+from dagster_essentials.defs.jobs import weekly_update_job
+
+weekly_update_schedule = dg.ScheduleDefinition(
+    job=weekly_update_job,
+    cron_schedule="0 0 * * 1", # every Monday at midnight
+)
