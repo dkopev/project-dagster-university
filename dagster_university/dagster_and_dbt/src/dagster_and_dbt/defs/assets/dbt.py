9baaf37180974a2d87d93b6bc870ca5cc55ff8c9
dkopev <d.kopev@devnet.de>
2026-05-28 09:32:55 +0000

    dependencies and stuff

---
+++ dagster_university/dagster_and_dbt/src/dagster_and_dbt/defs/assets/dbt.py
@@ -0,0 +1,26 @@
+# src/dagster_and_dbt/defs/assets/dbt.py
+import dagster as dg
+from dagster_dbt import dbt_assets, DbtCliResource
+
+from dagster_and_dbt.defs.project import dbt_project
+
+From the dagster_dbt module, import DagsterDbtTranslator
+
+From the dagster module, import AssetKey
+
+class CustomizedDagsterDbtTranslator(DagsterDbtTranslator):
+    def get_asset_key(self, dbt_resource_props):
+        resource_type = dbt_resource_props["resource_type"]
+        name = dbt_resource_props["name"]
+
+        if resource_type == "source":
+            return dg.AssetKey(f"taxi_{name}")
+        else:
+            return super().get_asset_key(dbt_resource_props)
+
+@dbt_assets(
+    manifest=dbt_project.manifest_path, 
+    dagster_dbt_translator=CustomizedDagsterDbtTranslator(),
+)
+def dbt_analytics(context: dg.AssetExecutionContext, dbt: DbtCliResource):
+    yield from dbt.cli(["build"], context=context).stream()
